3e81a6b92b146eb8115b6ad8ce65664d93d1964b
Swagnik02 <swagnik1234@gmail.com>
2023-12-12 00:46:09 +0530

    changes

--- lib/home/products/products_page.dart
+++ lib/home/products/products_page.dart
@@ -1,18 +1,63 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:m_soko/common/colors.dart';
 
 class ProductsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Products'),
-      ),
-      body: Center(
-        child: Text(
-          'PRODUCTS',
-          style: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.bold,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(8, 15, 8, 10),
+          child: Column(
+            children: [
+              // search box
+              Container(
+                height: 60,
+                decoration: BoxDecoration(
+                  color: ColorConstants.blue50,
+                  border: Border.all(color: ColorConstants.blue200),
+                ),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Icon(CupertinoIcons.search,
+                          color: ColorConstants.blue700),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Text(
+                          "Search for products",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: ColorConstants.blue700,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.camera_alt_outlined,
+                        color: ColorConstants.blue700,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Icon(CupertinoIcons.mic,
+                          color: ColorConstants.blue700),
+                    ),
+                  ],
+                ),
+              ),
+              // categories
+            ],
           ),
         ),
       ),
