28582d19e69521a0cc00975722379efb4c821557
Swagnik02 <swagnik1234@gmail.com>
2023-12-13 21:25:31 +0530

    miniProfile and PrimaryInfo added

--- lib/home/products/screens/bottomNavigationItems/profile_page.dart
+++ lib/home/products/screens/bottomNavigationItems/profile_page.dart
@@ -1,16 +1,39 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'package:m_soko/authentication/auth_services/auth_service.dart';
 import 'package:m_soko/common/colors.dart';
 
 class ProfilePage extends StatelessWidget {
+  String get userId => AuthService.firebase().currentUser!.id;
+  String get userName => AuthService.firebase().currentUser!.name;
+  String get email => AuthService.firebase().currentUser!.email;
+
+  // String get mobile => AuthService.firebase().currentUser!.mobile;
+  String mobile = '+27 9034566774';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
+            // App Bar
             _customAppBar(),
-            Expanded(
-              child: _profileBody(),
+            SizedBox(height: 10),
+
+            // main body
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  _miniProfile(),
+                  SizedBox(height: 16),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: _aboutSection(),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -18,9 +41,75 @@ class ProfilePage extends StatelessWidget {
     );
   }
 
-  Widget _profileBody() {
-    // Implement your profile body here
-    return Container();
+  Widget _miniProfile() {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.white,
+        ),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Material(
+        elevation: 5,
+        borderRadius: BorderRadius.circular(8),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Image.asset('assets/def_profile.png'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hello $userName!',
+                    style: const TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    mobile,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _aboutSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Primary Information'),
+        SizedBox(height: 10),
+        Row(
+          children: [
+            Icon(CupertinoIcons.device_phone_portrait),
+            SizedBox(width: 5),
+            Text(mobile),
+          ],
+        ),
+        SizedBox(height: 3),
+        Row(
+          children: [
+            Icon(CupertinoIcons.mail),
+            SizedBox(width: 5),
+            Text(email),
+          ],
+        ),
+      ],
+    );
   }
 
   Widget _customAppBar() {
