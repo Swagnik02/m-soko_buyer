5d42f833c561d74c7d5935b9689c9be1d0492acd
Swagnik02 <swagnik1234@gmail.com>
2023-12-13 21:50:55 +0530

    Profile Page UI completed

--- lib/home/products/screens/bottomNavigationItems/profile_page.dart
+++ lib/home/products/screens/bottomNavigationItems/profile_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 import 'package:m_soko/authentication/auth_services/auth_service.dart';
+import 'package:m_soko/authentication/auth_services/bloc/auth_bloc.dart';
+import 'package:m_soko/authentication/auth_services/bloc/auth_event.dart';
 import 'package:m_soko/common/colors.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -11,6 +15,12 @@ class ProfilePage extends StatelessWidget {
 
   // String get mobile => AuthService.firebase().currentUser!.mobile;
   String mobile = '+27 9034566774';
+  String city = 'Kolkata';
+  String pin = '731303';
+  String state = 'West Bengal';
+  String country = 'India';
+  String n = '4';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,8 +39,12 @@ class ProfilePage extends StatelessWidget {
                   _miniProfile(),
                   SizedBox(height: 16),
                   Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: _aboutSection(),
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      children: [
+                        _aboutSection(context),
+                      ],
+                    ),
                   ),
                 ],
               ),
@@ -50,7 +64,7 @@ class ProfilePage extends StatelessWidget {
         borderRadius: BorderRadius.circular(8),
       ),
       child: Material(
-        elevation: 5,
+        elevation: 2,
         borderRadius: BorderRadius.circular(8),
         child: Row(
           children: [
@@ -86,7 +100,7 @@ class ProfilePage extends StatelessWidget {
     );
   }
 
-  Widget _aboutSection() {
+  Widget _aboutSection(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.center,
@@ -107,6 +121,47 @@ class ProfilePage extends StatelessWidget {
             SizedBox(width: 5),
             Text(email),
           ],
+        ),
+        SizedBox(height: 25),
+        Text('Address'),
+        SizedBox(height: 10),
+        Text('$city $pin'),
+        SizedBox(height: 5),
+        Text('$state, $country'),
+
+        // Activity
+
+        SizedBox(height: 25),
+        Text('Activity'),
+        SizedBox(height: 10),
+        InkWell(
+          onTap: () {
+            Fluttertoast.showToast(msg: 'My Review');
+          },
+          child: Text('My Review'),
+        ),
+        SizedBox(height: 3),
+        Text('$n Messages Sent'),
+
+        // Manage Accounts
+        SizedBox(height: 25),
+        Text('Manage Accounts'),
+        SizedBox(height: 10),
+        InkWell(
+          onTap: () {
+            Fluttertoast.showToast(msg: 'Add Accounts');
+          },
+          child: Text('Add Accounts'),
+        ),
+        SizedBox(height: 3),
+        InkWell(
+          onTap: () {
+            context.read<AuthBloc>().add(
+                  const AuthEventLogOut(),
+                );
+            Fluttertoast.showToast(msg: 'Logged Out');
+          },
+          child: Text('Log Out'),
         ),
       ],
     );
