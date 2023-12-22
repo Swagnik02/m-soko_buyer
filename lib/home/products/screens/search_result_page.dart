aa600162741abef5a7e5940b5920a1cbd81340d4
Swagnik02 <swagnik1234@gmail.com>
2023-12-23 02:48:08 +0530

    searching

--- lib/home/products/screens/search_result_page.dart
+++ lib/home/products/screens/search_result_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:m_soko/home/products/widgets/product_thumbnails.dart';
 
 class SearchResultPage extends StatelessWidget {
   final String searchKeyword;
@@ -48,28 +49,75 @@ class SearchResultPage extends StatelessWidget {
       ),
       body: FutureBuilder<List<Map<String, dynamic>>>(
         future: futureCheckSelectedCategoryProducts(searchKeyword),
-        builder: (context, snapshot) {
+        builder: (context, snapshot)
+
+            // Product Thumbnail
+
+            {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator();
+            return const CircularProgressIndicator();
+          } else if (snapshot.data == null || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No data found'));
           } else if (snapshot.hasError) {
             return Text('Error: ${snapshot.error}');
           } else {
-            List<Map<String, dynamic>> searchResults = snapshot.data!;
-            // Render your search results using searchResults
-            return ListView.builder(
-              itemCount: searchResults.length,
-              itemBuilder: (context, index) {
-                var result = searchResults[index];
-                // Build your result item UI here
-                return ListTile(
-                  title: Text(result['itemName']),
-                  subtitle: Text(result['itemSubCategory']),
-                  // Add more details as needed
-                );
-              },
+            List<Map<String, dynamic>> products = snapshot.data!;
+            return Container(
+              // color: Colors.red,
+              height: (products.length / 2) * 700,
+              child: GridView.builder(
+                primary: false,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.58,
+                  crossAxisSpacing: 18.0,
+                  mainAxisSpacing: 20.0,
+                ),
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  return ProductThumbnail(
+                    itemPid: (products[index]['pid']),
+                    itemThumbnail: (products[index]['itemThumbnail']),
+                    itemSubCategory: (products[index]['itemSubCategory']),
+                    itemName: (products[index]['itemName']),
+                    itemMrp: (products[index]['itemMrp']),
+                    itemShippingCharge:
+                        (products[index]['itemShippingCharge']).toString(),
+                    itemDiscountPercentage: (products[index]
+                        ['itemDiscountPercentage']),
+                    itemOrderCount:
+                        (products[index]['itemOrderCount']).toString(),
+                  );
+                },
+              ),
             );
           }
         },
+
+        // ListView
+        // {
+        //   if (snapshot.connectionState == ConnectionState.waiting) {
+        //     return CircularProgressIndicator();
+        //   } else if (snapshot.hasError) {
+        //     return Text('Error: ${snapshot.error}');
+        //   } else {
+        //     List<Map<String, dynamic>> searchResults = snapshot.data!;
+        //     // Render your search results using searchResults
+        //     return ListView.builder(
+        //       itemCount: searchResults.length,
+        //       itemBuilder: (context, index) {
+        //         var result = searchResults[index];
+        //         // Build your result item UI here
+        //         return ListTile(
+        //           title: Text(result['itemName']),
+        //           subtitle: Text(result['itemSubCategory']),
+        //           // Add more details as needed
+        //         );
+        //       },
+        //     );
+        //   }
+        // },
       ),
     );
   }
