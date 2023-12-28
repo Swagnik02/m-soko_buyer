3fa2afbed0ab1fcf30c3b96488fba2754001fdcc
Swagnik02 <swagnik1234@gmail.com>
2023-12-28 12:02:09 +0530

    added exceptions to auth

--- lib/authentication/Views/registerView/register_controller.dart
+++ lib/authentication/Views/registerView/register_controller.dart
@@ -3,8 +3,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:logger/logger.dart';
+import 'package:m_soko/authentication/auth_exceptions.dart';
+import 'package:m_soko/routes/app_routes.dart';
 
 class RegisterController extends GetxController {
   late final TextEditingController name;
@@ -43,6 +46,12 @@ class RegisterController extends GetxController {
   }
 
   Future<void> createUser() async {
+    if (name.text.isEmpty ||
+        email.text.isEmpty ||
+        password.text.isEmpty ||
+        confirmPassword.text.isEmpty) {
+      throw EmptyFieldException('All fields');
+    }
     try {
       await FirebaseAuth.instance
           .createUserWithEmailAndPassword(
@@ -60,18 +69,24 @@ class RegisterController extends GetxController {
             'email': value.user!.email,
           });
         }
+
+        FirebaseAuth.instance
+            .signOut()
+            .then((value) => Get.offAllNamed(AppRoutes.loginScreen));
+        Fluttertoast.showToast(
+            msg: 'Account Created', toastLength: Toast.LENGTH_SHORT);
       });
     } on FirebaseAuthException catch (e) {
       Logger().e(e);
-      // if (e.code == 'weak-password') {
-      //   throw WeakPasswordAuthException();
-      // } else if (e.code == 'email-already-in-use') {
-      //   throw EmailAlreadyInUseAuthException();
-      // } else if (e.code == 'invalid-email') {
-      //   throw InvalidEmailAuthException();
-      // } else {
-      //   throw GenericAuthException();
-      // }
+      if (e.code == 'weak-password') {
+        throw WeakPasswordException();
+      } else if (e.code == 'email-already-in-use') {
+        throw EmailAlreadyInUseException();
+      } else if (e.code == 'invalid-email') {
+        throw InvalidEmailException();
+      } else {
+        throw AuthException();
+      }
     }
   }
 }
