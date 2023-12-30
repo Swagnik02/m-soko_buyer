cface67ff01705d179248ae05a6a424f874e9de4
Swagnik02 <swagnik1234@gmail.com>
2023-12-31 03:15:04 +0530

    help/support page

--- lib/navigation/bottomNavigationItems/bottomSheetPage/help_support_page.dart
+++ lib/navigation/bottomNavigationItems/bottomSheetPage/help_support_page.dart
@@ -1,25 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HelpSupportPage extends StatelessWidget {
-  const HelpSupportPage({super.key});
+  const HelpSupportPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: ListView(
-            children: <Widget>[
-              ListTile(
-                leading: Icon(Icons.notifications_active_outlined),
-                title: Text('Notifications'),
-                onTap: () {},
-              ),
-            ],
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset('assets/help_support_banner.png'),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _linkTo('Visit', 'https://help.sokoni.com', () {
+                        print('Visit link tapped!');
+                      }),
+                      _linkTo('Email Us', 'customercare@sokoni.com', () {
+                        launchEmail('customercare@sokoni.com');
+                      }),
+                      _linkTo('Call Us', '+91 6257899906', () {
+                        launchCall('+91 6257899906');
+                      }),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  Widget _linkTo(String linkTo, String link, VoidCallback onTapAction) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '$linkTo:',
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          InkWell(
+            child: Text(
+              link,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            ),
+            onTap: onTapAction,
+          ),
+        ],
+      ),
+    );
+  }
+
+  void launchEmail(String email) async {
+    print('testing');
+    // final Uri _emailLaunchUri = Uri(
+    //   scheme: 'mailto',
+    //   path: email,
+    // );
+    // final String _emailLaunchUriString = _emailLaunchUri.toString();
+
+    // if (await canLaunch(_emailLaunchUriString)) {
+    //   await launch(_emailLaunchUriString);
+    // } else {
+    //   throw 'Could not launch $_emailLaunchUriString';
+    // }
+  }
+
+  void launchCall(String phoneNumber) async {
+    // final Uri _callLaunchUri = Uri(
+    //   scheme: 'tel',
+    //   path: phoneNumber,
+    // );
+    // final String _callLaunchUriString = _callLaunchUri.toString();
+
+    // if (await canLaunch(_callLaunchUriString)) {
+    //   await launch(_callLaunchUriString);
+    // } else {
+    //   throw 'Could not launch $_callLaunchUriString';
+    // }
   }
 }
