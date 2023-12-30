3914df1acf79cd138b55326e5adcabcc59e22ade
Swagnik02 <swagnik1234@gmail.com>
2023-12-31 02:28:55 +0530

    added about Page

--- lib/navigation/bottomNavigationItems/bottomSheetPage/about_us_page.dart
+++ lib/navigation/bottomNavigationItems/bottomSheetPage/about_us_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:m_soko/home/logout.dart';
 
 class AboutUsPage extends StatelessWidget {
   const AboutUsPage({super.key});
@@ -8,17 +9,31 @@ class AboutUsPage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: ListView(
-            children: <Widget>[
-              ListTile(
-                leading: Icon(Icons.notifications_active_outlined),
-                title: Text('Notifications'),
-                onTap: () {},
+            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Image.asset('assets/about_us_banner.png'),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 8),
+                    child: Column(
+                      children: [
+                        Text(
+                          'About Sokoni',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          lorem + '\n' + lorem + '\n' + lorem,
+                          textAlign: TextAlign.justify,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
-            ],
-          ),
-        ),
+            )),
       ),
     );
   }
