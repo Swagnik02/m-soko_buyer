8b7f9098416140b8d6b3d5b26dd26ed3171a0124
Swagnik02 <swagnik1234@gmail.com>
2023-12-31 23:16:55 +0530

    add new button in address

--- lib/navigation/bottomNavigationItems/bottomSheetPage/address_page.dart
+++ lib/navigation/bottomNavigationItems/bottomSheetPage/address_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:m_soko/common/colors.dart';
 
 class AddressPage extends StatelessWidget {
   const AddressPage({super.key});
@@ -8,14 +10,53 @@ class AddressPage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: ListView(
-            children: <Widget>[
-              ListTile(
-                leading: Icon(Icons.notifications_active_outlined),
-                title: Text('Notifications'),
-                onTap: () {},
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Column(
+            children: [
+              Container(
+                height: 60,
+                decoration: const BoxDecoration(
+                    color: ColorConstants.blue50,
+                    border: Border.symmetric(
+                        horizontal: BorderSide(
+                            color: ColorConstants.skyBlue, width: 1))),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: InkWell(
+                    onTap: () {
+                      Fluttertoast.showToast(msg: 'Add');
+                    },
+                    child: const Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 4),
+                          child: Icon(
+                            Icons.add_rounded,
+                            size: 20,
+                            color: ColorConstants.skyBlue,
+                          ),
+                        ),
+                        Text(
+                          'Add a new address',
+                          style: TextStyle(
+                              color: ColorConstants.skyBlue,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
               ),
+              // ListView(
+              //   children: <Widget>[
+              //     ListTile(
+              //       leading: Icon(Icons.notifications_active_outlined),
+              //       title: Text('Notifications'),
+              //       onTap: () {},
+              //     ),
+              //   ],
+              // ),
             ],
           ),
         ),
