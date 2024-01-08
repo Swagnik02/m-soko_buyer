7259c98ace0efa77e3985cf899eabf5bbeb3cdbc
Swagnik02 <swagnik1234@gmail.com>
2024-01-09 02:12:30 +0530

    onboardpage responsive ui

--- lib/authentication/onBoardingScreen/onboard_page_widget.dart
+++ lib/authentication/onBoardingScreen/onboard_page_widget.dart
@@ -25,63 +25,31 @@ class OnboardPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final onBoardingController = Get.find<OnBoardingController>();
     return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
+      // mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        const SizedBox(height: 24),
-        Image.asset(imageUrl),
-        const SizedBox(height: 10),
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 5),
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            children: [
-              _buildListItem(desc1),
-              _buildListItem(desc2),
-              _buildListItem(desc3),
-            ],
-          ),
+        Image.asset(
+          imageUrl,
+          width: MediaQuery.of(context).size.width,
         ),
         const SizedBox(height: 10),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 40),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  onBoardingController.nextButtonFunc(context);
-                },
-                child: Container(
-                  width: 150,
-                  height: 50,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: ColorConstants.orange500,
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        pageIndex == 2 ? 'Get Started' : 'Next',
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      const Icon(
-                        Icons.arrow_forward_sharp,
-                        color: Colors.white,
-                      ),
-                    ],
-                  ),
-                ),
+        Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                children: [
+                  _buildListItem(desc1),
+                  _buildListItem(desc2),
+                  _buildListItem(desc3),
+                ],
               ),
-            ],
-          ),
-        )
+            ),
+            const SizedBox(height: 10),
+            // next / getStarted button
+            _buidButton(onBoardingController, context),
+          ],
+        ),
       ],
     );
   }
@@ -104,6 +72,50 @@ class OnboardPage extends StatelessWidget {
                 color: Colors.white,
               ),
               softWrap: true,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buidButton(
+      OnBoardingController onBoardingController, BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          GestureDetector(
+            onTap: () {
+              onBoardingController.nextButtonFunc(context);
+            },
+            child: Container(
+              width: 150,
+              height: 50,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: ColorConstants.orange500,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    pageIndex == 2 ? 'Get Started' : 'Next',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  const Icon(
+                    Icons.arrow_forward_sharp,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
             ),
           ),
         ],
